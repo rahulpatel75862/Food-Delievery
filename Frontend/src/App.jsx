f1f2bf3459fb4d8e4f1b008bb4054c06029e5f3e
rahulpatel75862 <patelrahul9533@gmail.com>
2025-09-05 06:43:29 +0530

    Signup page with backend functionality created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+export const serverUrl = "http://localhost:5000";
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/signup' element={<SignUp/>} />
+      <Route path='/signin' element={<SignIn/>} />
+    </Routes>
   )
 }
 
---
+++ Frontend/src/pages/SignUp.jsx
@@ -0,0 +1,185 @@
+import React, { useState } from "react";
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { serverUrl } from "../App";
+
+const signUp = () => {
+  const primaryColor = "#ff4d2d";
+  const hoverColor = "#e64323";
+  const bgColor = "#fff9f6";
+  const borderColor = "#ddd";
+  const [showPassword, setShowPassword] = useState(false);
+  const [role, setRole] = useState("user");
+  const navigate = useNavigate();
+
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [mobile, setMobile] = useState("");
+  
+  const handleSignUp = async () => {
+    try {
+      const result = await axios.post(
+        `${serverUrl}/api/auth/signup`,
+        {
+          fullName,
+          email,
+          password,
+          mobile,
+          role,
+        },
+        { withCredentials: true }
+      );
+      setFullName("")
+      setEmail("");
+      setMobile("")
+      setPassword("")
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  return (
+    <div
+      className="min-h-screen w-full flex items-center justify-center p-4"
+      style={{ backgroundColor: bgColor }}
+    >
+      <div
+        className={`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border-[1px]`}
+        style={{ border: `1px solid ${borderColor}` }}
+      >
+        <h1
+          className={`text-3xl font-bold mb-2`}
+          style={{ color: primaryColor }}
+        >
+          Sando
+        </h1>
+        <p className="text-gray-600 mb-8">
+          Create your account to get started with delicious food delieveries.
+        </p>
+        <div className="mb-4">
+          <label
+            htmlFor="fullName"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            FullName
+          </label>
+          <input
+            type="text"
+            className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500"
+            placeholder="Enter your full name"
+            style={{ border: `1px solid ${borderColor}` }}
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500"
+            placeholder="Enter your Email"
+            style={{ border: `1px solid ${borderColor}` }}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="phoneNo"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            Phone No.
+          </label>
+          <input
+            type="text"
+            className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500"
+            placeholder="Enter your Phone Number"
+            style={{ border: `1px solid ${borderColor}` }}
+            value={mobile}
+            onChange={(e) => setMobile(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            Password
+          </label>
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500 "
+              placeholder="Enter your full name"
+              style={{ border: `1px solid ${borderColor}` }}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              className="absolute right-3 top-3 text-gray-500 cursor-pointer"
+              onClick={() => setShowPassword((prev) => !prev)}
+            >
+              {!showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+            </button>
+          </div>
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="role"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            Role
+          </label>
+          <div className="flex gap-2">
+            {["user", "owner", "DelieveryBoy"].map((r) => (
+              <button
+                className="flex-1 border rounded-lg px-3 py-2 text-center font-medium transition-colors cursor-pointer"
+                onClick={() => setRole(r)}
+                style={
+                  role == r
+                    ? { backgroundColor: primaryColor, color: "white" }
+                    : {
+                        border: `1px solid ${primaryColor}`,
+                        color: primaryColor,
+                      }
+                }
+              >
+                {r}
+              </button>
+            ))}
+          </div>
+        </div>
+        <button
+          className={`w-full font-semibold py-2 rounded-lg transition duration-200 bg-[#ff4d2d] text-white hover:bg-[#e64323] cursor-pointer`}
+          onClick={handleSignUp}
+        >
+          Sign Up
+        </button>
+        <button className="w-full mt-4 flex items-center justify-center gap-2 border rounded-lg px-4 py-2 transition duration-200 border-gray-400 hover:bg-gray-100 cursor-pointer">
+          <FcGoogle size={20} />
+          <span>Sign up with Google</span>
+        </button>
+        <p className="text-center mt-6">
+          Already have an account?{" "}
+          <span
+            className="text-[#ff4d2d] cursor-pointer"
+            onClick={() => navigate("/signin")}
+          >
+            Sign in
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default signUp;
